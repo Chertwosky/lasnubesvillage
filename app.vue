eee2ca6bb77b75f1a1616c8d190f9cae5f716448
Chertwosky <4er2skygood@gmail.com>
2025-12-13 15:00:01 +0300

    Обновил блоки_10

--- app.vue
+++ app.vue
@@ -7,11 +7,56 @@ import PromoSlider from '@/components/blocks/PromoSlider.vue'
 import '@/assets/styles/global.css'
 import { useHead } from '#imports'
 
-// Скрипт bnovo грузим один раз
+const YM_ID = 105754948
+
 useHead({
   script: [
-    { src: '//widget.reservationsteps.ru/js/bnovo.js', defer: true }
-  ]
+    // Bnovo
+    {
+      src: '//widget.reservationsteps.ru/js/bnovo.js',
+      defer: true
+    },
+
+    // Yandex.Metrika
+    {
+      key: 'yandex-metrika-init',
+      type: 'text/javascript',
+      innerHTML: `
+        (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}
+          m[i].l=1*new Date()
+          for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) { return }
+          }
+          k=e.createElement(t)
+          a=e.getElementsByTagName(t)[0]
+          k.async=1
+          k.src=r
+          a.parentNode.insertBefore(k,a)
+        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
+
+        ym(${YM_ID}, 'init', {
+          ssr: true,
+          webvisor: true,
+          clickmap: true,
+          ecommerce: 'dataLayer',
+          accurateTrackBounce: true,
+          trackLinks: true
+        })
+      `
+    }
+  ],
+  noscript: [
+    {
+      key: 'yandex-metrika-noscript',
+      children: '<div><img src="https://mc.yandex.ru/watch/105754948" style="position:absolute; left:-9999px;" alt="" /></div>'
+    }
+  ],
+  // чтобы Nuxt не вырезал innerHTML
+  __dangerouslyDisableSanitizersByTagID: {
+    'yandex-metrika-init': ['innerHTML'],
+    'yandex-metrika-noscript': ['innerHTML']
+  }
 })
 </script>
 
@@ -26,7 +71,6 @@ useHead({
     <AppFooter />
 
     <PromoSlider />
-    <!-- модалка подключается глобально -->
     <BookingModal />
     <RulesModal />
   </div>
